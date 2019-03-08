6a1f1358b52aba5d6347fdb1c726e00e63338634
Huy Hoang <hoang@gkxim.com>
2019-03-08 14:35:55 +0700

    On finish AddToDoViewController

---
+++ ToDo List/AddToDoViewController.swift
@@ -0,0 +1,28 @@
+//
+//  AddToDoViewController.swift
+//  ToDo List
+//
+//  Created by Huy Hoang on 3/8/19.
+//  Copyright © 2019 Huy Hoang. All rights reserved.
+//
+
+import UIKit
+
+class AddToDoViewController: UIViewController {
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    
+    @IBOutlet weak var importantSwitch: UISwitch!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func addTapped(_ sender: Any) {
+    }
+    
+}
---
+++ ToDo List/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  ToDo List
+//
+//  Created by Huy Hoang on 3/8/19.
+//  Copyright © 2019 Huy Hoang. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
